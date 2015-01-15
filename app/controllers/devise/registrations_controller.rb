cb48c4ca3343804288ea96aeda8ab07a109f9fab
Landon Mossburg <wonderbeast@gmail.com>
2015-01-15 15:52:38 -0800

    disable sign up

--- app/controllers/devise/registrations_controller.rb
+++ app/controllers/devise/registrations_controller.rb
@@ -5,38 +5,45 @@ class Devise::RegistrationsController < DeviseController
 
   # GET /resource/sign_up
   def new
-    build_resource({})
-    @validatable = devise_mapping.validatable?
-    if @validatable
-      @minimum_password_length = resource_class.password_length.min
-    end
-    respond_with self.resource
+    flash[:info] = invite_only_text
+    redirect_to root_path
+
+    ###Disable Sign Up for Beta###
+    # build_resource({})
+    # @validatable = devise_mapping.validatable?
+    # if @validatable
+    #   @minimum_password_length = resource_class.password_length.min
+    # end
+    # respond_with self.resource
   end
 
   # POST /resource
   def create
-    build_resource(sign_up_params)
+    flash[:info] = invite_only_text
+    redirect_to root_path
+  end
+    # build_resource(sign_up_params)
 
-    resource_saved = resource.save
-    yield resource if block_given?
-    if resource_saved
-      if resource.active_for_authentication?
-        set_flash_message :notice, :signed_up if is_flashing_format?
-        sign_up(resource_name, resource)
-        respond_with resource, location: after_sign_up_path_for(resource)
-      else
-        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
-        expire_data_after_sign_in!
-        respond_with resource, location: after_inactive_sign_up_path_for(resource)
-      end
-    else
-      clean_up_passwords resource
-      @validatable = devise_mapping.validatable?
-      if @validatable
-        @minimum_password_length = resource_class.password_length.min
-      end
-      respond_with resource
-    end
+    # resource_saved = resource.save
+    # yield resource if block_given?
+    # if resource_saved
+    #   if resource.active_for_authentication?
+    #     set_flash_message :notice, :signed_up if is_flashing_format?
+    #     sign_up(resource_name, resource)
+    #     respond_with resource, location: after_sign_up_path_for(resource)
+    #   else
+    #     set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+    #     expire_data_after_sign_in!
+    #     respond_with resource, location: after_inactive_sign_up_path_for(resource)
+    #   end
+    # else
+    #   clean_up_passwords resource
+    #   @validatable = devise_mapping.validatable?
+    #   if @validatable
+    #     @minimum_password_length = resource_class.password_length.min
+    #   end
+    #   respond_with resource
+    # end
   end
 
   # GET /resource/edit
@@ -145,5 +152,9 @@ class Devise::RegistrationsController < DeviseController
 
   def account_update_params
     devise_parameter_sanitizer.sanitize(:account_update)
+  end
+
+  def invite_only_text
+    "Sorry, we're in closed beta right now and are not accepting invites.  If you'd like to request an invite to the beta program email us at team@trym.io."
   end
 end
