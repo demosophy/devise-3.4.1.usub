12964c20eeff60b951e5604ab9eaf6070f5e3ccb
Landon Mossburg <wonderbeast@gmail.com>
2015-07-12 14:21:57 -0700

    re-enable sign up

--- app/controllers/devise/registrations_controller.rb
+++ app/controllers/devise/registrations_controller.rb
@@ -5,46 +5,44 @@ class Devise::RegistrationsController < DeviseController
 
   # GET /resource/sign_up
   def new
-    flash[:info] = invite_only_text
-    redirect_to root_path
+    # flash[:info] = invite_only_text
+    # redirect_to root_path
 
-    ###Disable Sign Up for Beta###
-    # build_resource({})
-    # @validatable = devise_mapping.validatable?
-    # if @validatable
-    #   @minimum_password_length = resource_class.password_length.min
-    # end
-    # respond_with self.resource
+    build_resource({})
+    @validatable = devise_mapping.validatable?
+    if @validatable
+      @minimum_password_length = resource_class.password_length.min
+    end
+    respond_with self.resource
   end
 
   # POST /resource
   def create
-    flash[:info] = invite_only_text
-    redirect_to root_path
+    # flash[:info] = invite_only_text
+    # redirect_to root_path
 
-    ###Disable Sign Up for Beta###
-    # build_resource(sign_up_params)
+    build_resource(sign_up_params)
 
-    # resource_saved = resource.save
-    # yield resource if block_given?
-    # if resource_saved
-    #   if resource.active_for_authentication?
-    #     set_flash_message :notice, :signed_up if is_flashing_format?
-    #     sign_up(resource_name, resource)
-    #     respond_with resource, location: after_sign_up_path_for(resource)
-    #   else
-    #     set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
-    #     expire_data_after_sign_in!
-    #     respond_with resource, location: after_inactive_sign_up_path_for(resource)
-    #   end
-    # else
-    #   clean_up_passwords resource
-    #   @validatable = devise_mapping.validatable?
-    #   if @validatable
-    #     @minimum_password_length = resource_class.password_length.min
-    #   end
-    #   respond_with resource
-    # end
+    resource_saved = resource.save
+    yield resource if block_given?
+    if resource_saved
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_flashing_format?
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      @validatable = devise_mapping.validatable?
+      if @validatable
+        @minimum_password_length = resource_class.password_length.min
+      end
+      respond_with resource
+    end
   end
 
   # GET /resource/edit
